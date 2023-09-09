35879b174a6011ad20727c62a744355d76b6263e
irfan9814 <74260414+irfan9814@users.noreply.github.com>
2023-09-09 17:51:23 +0530

    added  second home page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <img src='https://www.instacart.com/assets/beetstrap/brand/2022/instacart-logo-color-6678cb82d531f8910d5ba270a11a7e9b56fc261371bda42ea7a5abeff3492e1c.svg' />
+      <h1>Second Home Page</h1>
     </div>
   );
 }
