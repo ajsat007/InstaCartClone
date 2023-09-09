20b7a2835b1d1ae9ece4a2afe9bc3bcec1b230dc
irfan9814 <74260414+irfan9814@users.noreply.github.com>
2023-09-09 17:24:53 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src='https://www.instacart.com/assets/beetstrap/brand/2022/instacart-logo-color-6678cb82d531f8910d5ba270a11a7e9b56fc261371bda42ea7a5abeff3492e1c.svg' />
+    </div>
+  );
+}
+
+export default App;
