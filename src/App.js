47beb044eabab71dac9d060567b7d50e98ac26e3
irfan9814 <74260414+irfan9814@users.noreply.github.com>
2023-09-09 18:01:04 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,9 @@ function App() {
   return (
     <div className="App">
       <img src='https://www.instacart.com/assets/beetstrap/brand/2022/instacart-logo-color-6678cb82d531f8910d5ba270a11a7e9b56fc261371bda42ea7a5abeff3492e1c.svg' />
-<<<<<<< HEAD
-      <h1>Second Home Page</h1>
-=======
-      <p>Home Page</p>
->>>>>>> 9482afa36e44bffd85d5aa9b590fccd3adb0cc04
+
+      <h1>Home Page</h1>
+
     </div>
   );
 }
