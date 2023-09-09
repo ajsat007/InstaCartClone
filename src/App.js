1c0ccb3598f46d570c84f453c629647b85b11d91
Ajinkya Satkar <ajinkyasatkar007@gmail.com>
2023-09-09 18:08:31 +0530

    j

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <img src='https://www.instacart.com/assets/beetstrap/brand/2022/instacart-logo-color-6678cb82d531f8910d5ba270a11a7e9b56fc261371bda42ea7a5abeff3492e1c.svg' />
 
-
+      <p>Home Page</p>
     </div>
   );
 }
